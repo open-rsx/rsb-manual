8c9372cda57510446159e6911c00bc4c59d01047
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-10-23 13:33:57 +0200

    Improved jQuery-based filtering in _static/multi.js
    Previously, the jQuery filters for containers were too restrictive and
    could yield false positives and false negatives.
    
    * _static/multi.js: when finding containers, look for classes "multi"
      and "container" independently; when finding child containers, make
      sure that their class contains the name of the parent container

--- _static/multi.js
+++ _static/multi.js
@@ -2,19 +2,19 @@
 //
 // A multi-variant section is created by using nested .. container::
 // directives as follows:
-// 
+//
 // .. container:: NAME-multi
-// 
+//
 //    .. container:: NAME-python
-// 
+//
 //       CONTENT
-// 
+//
 //    .. container:: NAME-cpp
-// 
+//
 //       CONTENT
-// 
+//
 //    MORE-VARIANTS
-// 
+//
 // The "multi" string is mandatory. NAME and the -python, -cpp,
 // etc. suffixes are arbitrary.
 
@@ -30,15 +30,15 @@ function switchMulti(parent, all, switchTo) {
 }
 
 $(document).ready(function () {
-  jQuery.map($('*[class*="multi container"]'), function (parent) {
+  jQuery.map($('*[class*="multi"]').filter('*[class*="container"]'), function (parent) {
     //
-    parentName = parent.className.replace(/^(.*)-multi container$/, '$1');
-
-    all = jQuery.map($(parent).find('*[class*="container"]'), function (node) {
-      clazz = node.className;
-      return {'name':  clazz.replace(/^(.*) container$/, '$1'),
-              'label': clazz.replace(/^.*-([^-].*) container$/, '$1') };
-    })
+    parentName = parent.className.replace(/^(.*)-multi .*container$/, '$1');
+    all = jQuery.map($(parent).find('*[class*="container"]').filter('*[class*="' + parentName + '"]'),
+                     function (node) {
+                         clazz = node.className;
+                         return { 'name':  clazz.replace(/^(.*) container$/, '$1'),
+                                  'label': clazz.replace(/^.*-([^-].*) container$/, '$1') };
+                     })
     allNames = jQuery.map(all, function (nameAndLabel) { return nameAndLabel.name });
 
     code = jQuery.map(all, function (nameAndLabel) {
